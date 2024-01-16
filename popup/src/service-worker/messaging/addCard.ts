5102c003f30d8aa116da074eabd9f45d34a0a057
Jack <fawthorp878@gmail.com>
2024-01-16 21:29:56 +1300

    Added error alert for when card doesn't save #23 (#31)

--- popup/src/service-worker/messaging/addCard.ts
+++ popup/src/service-worker/messaging/addCard.ts
@@ -1,4 +1,23 @@
-export const handleAddCard = (fields: any) => {
+import { AddCardResponse } from "../../types/ResponseTypes";
+
+type ankiResponse = {
+    error: string | null;
+    result: number | null;
+}
+
+const validateResponse = (
+    response:ankiResponse,
+    resolve: any, 
+    reject: any) => {
+    if (response.error) {
+        console.log("Rejecting response");
+        reject(response.error);
+    } else {
+        resolve(response);
+    }
+}
+
+export const handleAddCard = (fields: any): Promise<AddCardResponse> => {
     const body = {
         "action": "addNote",
         "version": 6,
@@ -20,8 +39,6 @@ export const handleAddCard = (fields: any) => {
         }
     };
 
-    console.log(`Fields: ${JSON.stringify(fields)}`)
-
     return new Promise((resolve, reject) => {
         fetch("http://localhost:8765/", {
             method: 'POST',
@@ -31,7 +48,7 @@ export const handleAddCard = (fields: any) => {
             body: JSON.stringify(body),
     })
         .then(response => response.json())
-        .then(data => resolve(data))
+        .then(data => validateResponse(data, resolve, reject))
         .catch(error => reject(error));
     });
 }
